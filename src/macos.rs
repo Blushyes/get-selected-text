6aaf1f704ca3ad422bce87a4bbd23d1272c70eee
Blushyes <Blushyes@gmail.com>
2024-09-21 03:48:32 +0800

    time listen

--- src/macos.rs
+++ src/macos.rs
@@ -184,34 +184,7 @@ fn get_selected_text_by_ax() -> Result<String, Box<dyn std::error::Error>> {
 }
 
 const REGULAR_TEXT_COPY_APPLE_SCRIPT: &str = r#"
-use AppleScript version "2.4"
-use scripting additions
-use framework "Foundation"
-use framework "AppKit"
 
-set savedAlertVolume to alert volume of (get volume settings)
-
--- Back up clipboard contents:
-set savedClipboard to the clipboard
-
-set thePasteboard to current application's NSPasteboard's generalPasteboard()
-set theCount to thePasteboard's changeCount()
-
-
--- Copy selected text to clipboard:
-tell application "System Events" to keystroke "c" using {command down}
-delay 0.1 -- Without this, the clipboard may have stale data.
-
-
-if thePasteboard's changeCount() is theCount then
-    return ""
-end if
-
-set theSelectedText to the clipboard
-
-set the clipboard to savedClipboard
-
-theSelectedText
 "#;
 
 const FILE_PATH_COPY_APPLE_SCRIPT: &str = r#"
