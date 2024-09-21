a7a90a621d0499cba3949bdb27b3f885d970e698
Blushyes <Blushyes@gmail.com>
2024-09-22 02:44:09 +0800

    async

--- src/macos.rs
+++ src/macos.rs
@@ -172,16 +172,18 @@ use framework "AppKit"
 -- Back up clipboard contents:
 set savedClipboard to the clipboard
 
+delay 0.5
+
 set thePasteboard to current application's NSPasteboard's generalPasteboard()
--- set theCount to thePasteboard's changeCount()
+set theCount to thePasteboard's changeCount()
 
 -- Copy selected text to clipboard:
 -- tell application "System Events" to keystroke "c" using {command down}
-delay 0.5 -- Without this, the clipboard may have stale data.
+-- delay 0.5 -- Without this, the clipboard may have stale data.
 
--- if thePasteboard's changeCount() is theCount then
---     return ""
--- end if
+if thePasteboard's changeCount() is theCount then
+     return ""
+end if
 
 set theSelectedText to the clipboard
 
